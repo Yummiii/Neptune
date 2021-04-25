952f87840eef000cf442b07801848a79d4ae866b
Yummiii <Leeo05050@outlook.com>
2021-04-25 15:33:57 -0300

    ligar ou desligar as hotkey

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,50 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 use clipboard::{ClipboardContext, ClipboardProvider};
 use passwords::PasswordGenerator;
-use umiko::hotkeys::{HotKeys, KeyModifiers};
+use umiko::{common::Keys, hotkeys::{HotKeys, KeyModifiers}};
 use enigo::{Enigo, KeyboardControllable};
 use uuid::Uuid;
 
-fn main() {    
-    let mut hk = HotKeys::new();
-    hk.add(KeyModifiers::MOD_CONTROL, 'p', || {
-        if umiko::keys::is_locked(0x91) {
-            let senha = PasswordGenerator::new()
-                .length(50)
-                .lowercase_letters(true)
-                .uppercase_letters(true)
-                .symbols(true)
-                .numbers(true)
-                .generate_one()
-                .unwrap();
-            escrever(senha);
+fn main() {   
+    let mut hkhandler  = HotKeys::new();
+    registrar(&mut hkhandler);
+    hkhandler.handle();
+}
+
+fn registrar(handler: &mut HotKeys) {
+    let mut hotkeys = Vec::new();
+
+    hotkeys.push(handler.add(KeyModifiers::MOD_CONTROL, Keys::P, |_| {
+        let senha = PasswordGenerator::new()
+            .length(50)
+            .lowercase_letters(true)
+            .uppercase_letters(true)
+            .symbols(true)
+            .numbers(true)
+            .generate_one()
+            .unwrap();
+        escrever(senha);        
+    }));
+
+    hotkeys.push(handler.add(KeyModifiers::MOD_CONTROL, Keys::G, |_| {
+        escrever(Uuid::new_v4().to_string());
+    }));
+
+    hotkeys.push(handler.add(KeyModifiers::MOD_CONTROL, Keys::O, |_| {
+        escrever(SystemTime::now().duration_since(UNIX_EPOCH).expect("error").as_millis().to_string());
+    }));
+
+    handler.add(KeyModifiers::NONE_MODIFIES, Keys::SCROLL_LOCK, move |h| {
+        if umiko::keys::is_locked(Keys::SCROLL_LOCK) {
+            registrar(h);
+            println!("registradas");
+        } else {
+            for hk in hotkeys.iter() {
+                h.remove(*hk);
+            }
+            println!("Desregistradas");
         }
     });
-
-    hk.add(KeyModifiers::MOD_CONTROL, 'g', || {
-        if umiko::keys::is_locked(0x91) {
-            escrever(Uuid::new_v4().to_string());
-        };
-    });
-
-    hk.add(KeyModifiers::MOD_CONTROL, 'o', || {
-        if umiko::keys::is_locked(0x91) {
-            escrever(SystemTime::now().duration_since(UNIX_EPOCH).expect("error").as_millis().to_string());
-        };
-    });
-
-    hk.handle();
 }
 
 fn escrever(texto: String) {
