3179de961be1991c9ca54563b25fd75f4f5784c3
Yummiii <Leeo05050@outlook.com>
2021-05-13 16:08:11 -0300

    linux suicidio

---
+++ src/arguments.rs
@@ -0,0 +1,18 @@
+use clap::{Clap, AppSettings};
+
+#[derive(Clap, Debug)]
+#[clap(setting = AppSettings::ColoredHelp)]
+pub struct Options {
+    #[clap(short, long)]
+    pub password: bool,
+    #[clap(short, long)]
+    pub uuid: bool,
+    #[clap(short, long)]
+    pub timestamp: bool
+}
+
+impl Options {
+    pub fn build() -> Self {
+        Options::parse()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {   
+mod arguments;
 
+use arguments::Options;
+
+fn main() {
+    let args = Options::build();
+    
+    if args.password {
+        println!("Senha");
+    } else if args.timestamp {
+        println!("Timestamp");
+    } else if args.uuid {
+        println!("UUID");
+    }
 }
