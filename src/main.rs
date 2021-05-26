ea8d8b04c42861f4a2cfe4e1292be6c5b1edca09
Yummiii <Leeo05050@outlook.com>
2021-05-26 12:58:30 -0300

    tem alguma cosa explodindo e eu ainda n sei oq eh

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     stdin.read_to_end(&mut buf)?;
 
     let now = Utc::now();
-    let local = format!("/home/yummi/Taiga/Printis/{}-{:02}", now.year(), now.month());
+    let local = format!("/home/yummi/teste/{}-{:02}", now.year(), now.month());
     fs::create_dir_all(&local).await?;
     let quantidade = glob(&format!("{}/../**/*.*", &local))?.count() + 1;
     fs::write(format!("{}/{}.png", local, quantidade), buf).await?;
