19f9319c0c5fd7b23512b51ee9cd7b0693218e0a
Yummiii <Leeo05050@outlook.com>
2021-05-26 14:06:42 -0300

    talvez? eu tenha corrigido o erro, mas n tenho ctz

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,16 @@ use glob::glob;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
     let mut buf = Vec::new();
-    let mut stdin = io::stdin(); 
-    stdin.read_to_end(&mut buf)?;
+    handle.read_to_end(&mut buf)?;
+
 
     let now = Utc::now();
     let local = format!("/home/yummi/teste/{}-{:02}", now.year(), now.month());
     fs::create_dir_all(&local).await?;
     let quantidade = glob(&format!("{}/../**/*.*", &local))?.count() + 1;
     fs::write(format!("{}/{}.png", local, quantidade), buf).await?;
-
     Ok(())
 }
