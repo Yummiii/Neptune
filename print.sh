1ea077c3bfcc35365658d765eddbf9e6bb947b5b
Yummiii <Leeo05050@outlook.com>
2021-06-02 14:29:23 -0300

    agr o print cria pastas e n salva quando eh cancelado

--- print.sh
+++ print.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 local=~/Taiga/Printis/$(date +%Y-%m)/
+[ ! -d ${local} ] && mkdir ${local}
 local=${local}$(($(find ${local}.. -type f | wc -l)+1)).png
-flameshot gui -r > ${local}
+print=$(flameshot gui -r | base64)
+[ "$print" != "c2NyZWVuc2hvdCBhYm9ydGVkCg==" ] && echo "$print" | base64 --decode > "$local"
