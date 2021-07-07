3ff436b2f9f1b2b55c53b7170883c18d38947d58
Yummiii <Leeo05050@protonmail.com>
2021-07-07 04:33:23 -0300

    ta printando?

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,19 @@
-mod arguments;
-mod utils;
+mod cmds;
 
-use std::time::{SystemTime, UNIX_EPOCH};
-use arguments::{Options, SubCommands};
-use passwords::PasswordGenerator;
-use utils::enviar;
-use uuid::Uuid;
+use chrono::Utc;
+use std::fs;
+use cmds::quantidade_arquivos;
+use crate::cmds::print;
 
 pub type GenericError = Box<dyn std::error::Error + Send + Sync>;
 
 fn main() -> Result<(), GenericError> {
-    let args = Options::build();    
-    match args.subcmd {
-        SubCommands::Geradores(gerador) => {
-            if gerador.tipo == 1 {
-                let senha = PasswordGenerator::new()
-                    .length(50)
-                    .lowercase_letters(true)
-                    .uppercase_letters(true)
-                    .symbols(true)
-                    .numbers(true)
-                    .generate_one()?;
-                enviar(senha)?;
-            } else if gerador.tipo == 2 {
-                enviar(Uuid::new_v4().to_string())?;
-            } else if gerador.tipo == 3 {
-                enviar(SystemTime::now()
-                    .duration_since(UNIX_EPOCH)?
-                    .as_millis()
-                    .to_string())?;
-            }
-        },
-        SubCommands::Enviar(..) => {
-            println!("Enviar")
-        }
-    }
-
+    let mut local = Utc::now().format("/home/yummi/Taiga/%Y-%m/").to_string();
+    fs::create_dir_all(&local)?;
+    local.push_str(&format!(
+        "{}.png",
+        quantidade_arquivos(&format!("{}..", &local))? + 1
+    ));
+    print(&local)?;
     Ok(())
 }
