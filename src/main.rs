89ee0c7f9c5303dcdbe85f0d15f4ef3c54c1af47
Yummiii <Leeo05050@protonmail.com>
2022-01-24 00:36:05 -0300

    fazendo funcionar

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,12 @@ use run_script::ScriptOptions;
 pub type GenericError = Box<dyn std::error::Error + Send + Sync>;
 fn main() -> Result<(), GenericError> {
     let mut local = Utc::now().format(&args().nth(1).expect("Você não falou o pasta que tenho q salvar o print")).to_string();
-    
+    fs::create_dir_all(&local)?;
+
     let (quantidade, _) = run_script::run_script_or_exit!(r#"find $1../ -type f | wc -l"#, &vec![local.clone()], &ScriptOptions::new());
     let proximo = quantidade.trim().parse::<i32>().unwrap() + 1;
-
-    fs::create_dir_all(&local)?;
     local.push_str(&format!("{}.png", proximo));
 
-    let (_, _) = run_script::run_script_or_exit!(r#"flameshot gui -r > $1"#, &vec![local], &ScriptOptions::new());
+    run_script::run_script_or_exit!(r#"gnome-screenshot -a -c -f $1"#, &vec![local], &ScriptOptions::new());
     Ok(())    
 }
