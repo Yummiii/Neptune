bcb928c76ba0353dc224e4d4e9cc59a58ae6d96a
Yummiii <Leeo05050@protonmail.com>
2022-04-22 14:14:57 -0300

    gnome e suas brilhantes ideias

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,28 @@
-use std::fs::{File, self};
+use std::fs;
 use chrono::Utc;
-use daemonize::Daemonize;
 use fs_extra::{file::CopyOptions};
 use hotwatch::{blocking::{Hotwatch, Flow}, Event};
 use run_script::ScriptOptions;
 
-const WORK_DIR: &str = "/home/yummi/.nepnep/";
 
 fn main() {
-    fs::create_dir_all(format!("{}prints", WORK_DIR)).unwrap();
-    let stdout = File::create(format!("{}daemon.out", WORK_DIR)).unwrap();
-    let stderr = File::create(format!("{}daemon.err", WORK_DIR)).unwrap();
+    let work_dir: String = format!("{}/Capturas de tela", xdg_user::pictures().unwrap().unwrap().display());
 
-    let daemonize = Daemonize::new()
-        .working_directory(WORK_DIR)
-        .stdout(stdout)  
-        .stderr(stderr); 
+    fs::create_dir_all(&work_dir).unwrap();
+    let mut hotwatch = Hotwatch::new().expect("hotwatch failed to initialize!");
+    hotwatch.watch(work_dir, |event: Event| {
+        if let Event::Create(path) = event {
+            let mut local = Utc::now().format("/home/yummi/Taiga/Printis/%Y-%m/").to_string();
+            fs::create_dir_all(&local).unwrap();
 
-    match daemonize.start() {
-        Ok(_) => {
-            let mut hotwatch = Hotwatch::new().expect("hotwatch failed to initialize!");
-            hotwatch.watch(format!("{}prints/", WORK_DIR), |event: Event| {
-                println!("{:#?}", event);
-                if let Event::Create(path) = event {
-                    let mut local = Utc::now().format("/home/yummi/Taiga/Printis/%Y-%m/").to_string();
-                    fs::create_dir_all(&local).unwrap();
-    
-                    let (quantidade, _) = run_script::run_script_or_exit!(r#"find $1../ -type f | wc -l"#, &vec![local.clone()], &ScriptOptions::new());
-                    let proximo = quantidade.trim().parse::<i32>().unwrap() + 1;
-                    local.push_str(&format!("{}.png", proximo));
-                    
-                    println!("Original: {}\nPara: {}", path.to_str().unwrap(), &local);
-                    fs_extra::file::move_file(path, local, &CopyOptions::new()).unwrap();
-                }
-                Flow::Continue
-            }).unwrap();
-            hotwatch.run();
+            let (quantidade, _) = run_script::run_script_or_exit!(r#"find $1../ -type f | wc -l"#, &vec![local.clone()], &ScriptOptions::new());
+            let proximo = quantidade.trim().parse::<i32>().unwrap() + 1;
+            local.push_str(&format!("{}.png", proximo));
+            
+            println!("Original: {}\nPara: {}\n", path.to_str().unwrap(), &local);
+            fs_extra::file::move_file(path, local, &CopyOptions::new()).unwrap();
         }
-        Err(e) => eprintln!("Error, {}", e),
-    }
+        Flow::Continue
+    }).unwrap();
+    hotwatch.run();
 }
